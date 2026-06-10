2339069186d798041d90d30ce2039b47d763697f
egorklementev <egorklementeff@yandex.ru>
2026-06-10 17:48:13 +0300

    FEATURE: simple dragging

---
+++ Assets/The Last of Merge/Scripts/Core/Views/ItemSlotView.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(RectTransform))]
+public class ItemSlotView : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float dragSmoothTime = 0.1f;
+
+    [SerializeField]
+    private Canvas canvas; // TODO: inject this
+
+    private RectTransform rectTransform;
+    private Vector2 dragStartPosition;
+    private Vector2 clickStartPosition;
+    private ItemSlotState state = ItemSlotState.RESTING;
+
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        switch (state)
+        {
+            case ItemSlotState.RESTING:
+                break;
+            case ItemSlotState.DRAGGING:
+                break;
+            case ItemSlotState.RELEASED:
+                state = ItemSlotState.SNAPPING;
+                // TODO: make needed calculations here (checks)
+                break;
+            case ItemSlotState.SNAPPING:
+                state = ItemSlotState.RESTING; // TODO: wait for the snap animation and then change the state
+                break;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 screenDelta = eventData.position - clickStartPosition;
+        Vector2 canvasDelta = screenDelta / canvas.scaleFactor;
+        rectTransform.anchoredPosition = dragStartPosition + canvasDelta;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (state != ItemSlotState.RESTING)
+            return;
+
+        state = ItemSlotState.DRAGGING;
+        dragStartPosition = rectTransform.anchoredPosition;
+        clickStartPosition = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (state != ItemSlotState.DRAGGING)
+            return;
+
+        state = ItemSlotState.RELEASED;
+    }
+
+    public enum ItemSlotState
+    {
+        RESTING,
+        DRAGGING,
+        RELEASED,
+        SNAPPING,
+    }
+}
